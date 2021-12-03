7855404500ae69ec909fec0c94504fa5e8766464
eBanjo <git-ebj2@kir3.ddns.net>
2021-12-03 19:19:30 +0100

    Entity Product

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO categories (name) values('cat - 1');
+#
+INSERT INTO products (cikkszam, name) values('P/1', 'prod - 1');
+#
+INSERT INTO promotions (name) values('prom - 1');
+#
+INSERT INTO promotion_products (name) values('prom - 1');
